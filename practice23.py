25158ed3c2dfeed5d91c53b104be097a58e76b1d
Shraddha_Kharatmal_ <kharatmalshraddha55@gmail.com>
2026-04-23 21:16:04 +0530

    Add files via upload

---
+++ practice23.py
@@ -0,0 +1,102 @@
+##st="shraddha"
+##print(st[::-1])
+##
+##
+##rev=""
+##for ch in st:
+##    rev=ch+rev
+##
+##print(rev)
+
+#palindrome
+##st="madam"
+##if st==st[::-1]:
+##    print("palindrome")
+
+##st="shraa"
+##rev=""
+##
+##for ch in st:
+##    rev=ch+rev
+##
+##if rev==st:
+##    print("Palindrome")
+##else:
+##    print("Not Palindrome")
+
+
+#even odd
+##number=int(input("Enter the number:"))
+##if number%2==0:
+##    print("even")
+##else:
+##    print("Odd")
+
+
+
+##number palindrome
+##n=12321
+##temp=n
+##rev=0
+##
+##while temp>0:
+##    l_digit=temp%10
+##    rev=rev*10+l_digit
+##    temp//=10
+##
+##if rev==n:
+##    print("palindrome")
+##else:
+##    print("Not Palindrome")
+##
+##
+##
+####prime number
+##n=3
+##count=0
+##for i in range(2,n):
+##    if n%i==0:
+##        count+=1
+##
+##if count==0:
+##    print("Prime")
+##else:
+##    print("Not prime")
+##        
+##
+####prime
+##n=8
+##
+##for i in range(2,n):
+##    if n%i==0:
+##        print("not prime")
+##        break
+##    else:
+##        print("prime")
+
+
+##fibonacci
+n=10
+a,b=0,1
+
+for i in range(n):
+    print(a,end=" ")
+    a,b=b,a+b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
