625376f081607ab6efb5cbc64108fb1bd9dadb90
Shraddha_Kharatmal_ <kharatmalshraddha55@gmail.com>
2026-04-30 19:13:39 +0530

    Add files via upload

---
+++ dsa_array.py
@@ -0,0 +1,20 @@
+import array as arr
+from array import *
+
+
+
+
+
+
+
+
+val= arr.array('i',[1,2,3,4,4])
+print(val)
+
+for i in range(0,5):
+    print(val[i], end=" ")
+
+
+
+for x in val:
+    print(x, end="-")
